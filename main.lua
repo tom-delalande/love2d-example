af59d57207187bea4f2761f0dad3d0c6a979df94
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2024-11-17 09:22:54 +0800

    add momentum

--- main.lua
+++ main.lua
@@ -10,7 +10,30 @@ local card = {
         width = 126,
         height = 176
     },
+    target_transform = {
+        x = (screenWidth - 126) / 2,
+        y = (screenHeight - 176) / 2,
+        width = 126,
+        height = 176
+    },
+    velocity = {
+        x = 0,
+        y = 0,
+    }
 }
+
+local function move(card, dt)
+    local momentum = 0.75
+    if (card.target_transform.x ~= card.transform.x or card.velocity.x ~= 0) or
+        (card.target_transform.y ~= card.transform.y or card.velocity.y ~= 0) then
+        card.velocity.x = momentum * card.velocity.x +
+            (1 - momentum) * (card.target_transform.x - card.transform.x) * 30 * dt
+        card.velocity.y = momentum * card.velocity.y +
+            (1 - momentum) * (card.target_transform.y - card.transform.y) * 30 * dt
+        card.transform.x = card.transform.x + card.velocity.x
+        card.transform.y = card.transform.y + card.velocity.y
+    end
+end
 
 function love.load()
     cardSprite = love.graphics.newImage("card.png")
@@ -31,13 +54,15 @@ function love.mousepressed(x, y)
     end
 end
 
-function love.update()
+function love.update(delta_time)
     if card.dragging then
-        card.transform.x = love.mouse.getX() - card.transform.width / 2
-        card.transform.y = love.mouse.getY() - card.transform.height / 2
+        card.target_transform.x = love.mouse.getX() - card.transform.width / 2
+        card.target_transform.y = love.mouse.getY() - card.transform.height / 2
     end
+    move(card, delta_time)
 end
 
 function love.mousereleased()
     card.dragging = false
 end
+
